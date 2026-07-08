9e2f05e062f3fdb0811e2151fca5a096142b48b3
rewanshchoudhary57 <rewanshchoudhary57@gmail.com>
2026-07-08 23:35:28 +0530

    hmmm

---
+++ src/main/java/com/example/CloudDocs/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.CloudDocs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http){
+        return http.authorizeHttpRequests((requests)->
+                requests.requestMatchers("/").permitAll().anyRequest().authenticated()).formLogin((form)->
+                form.loginPage("/login").permitAll())
+                .logout(LogoutConfigurer::permitAll).build();
+
+
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+
+    }
+}
