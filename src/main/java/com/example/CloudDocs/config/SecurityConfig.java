6f2a8dc61d0e83d2e1d7a24d7d4085d10974a752
rewanshchoudhary57 <rewanshchoudhary57@gmail.com>
2026-07-14 01:56:14 +0530

    Running state

--- src/main/java/com/example/CloudDocs/config/SecurityConfig.java
+++ src/main/java/com/example/CloudDocs/config/SecurityConfig.java
@@ -1,35 +1,63 @@
 package com.example.CloudDocs.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
 
-    //private final JwtAuthFilter jwtAuthFilter;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/public/**", "/auth/**", "/login/**").permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated()
+                ).sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling((exs) ->
+                        exs.accessDeniedHandler((req, res, e) ->
+                                        res.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage()))
+                                .authenticationEntryPoint((req, res, e) -> {
+                                    res.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+                                })
+                );
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider daoAuth = new DaoAuthenticationProvider(userDetailsService);
+        daoAuth.setPasswordEncoder(passwordEncoder);
+        return daoAuth;
+    }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
         return config.getAuthenticationManager();
-
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-
     }
-
 }
-
