37c6332efc9185f1f88ac7d90a88918981c2a9de
rewanshchoudhary57 <rewanshchoudhary57@gmail.com>
2026-07-26 23:51:16 +0530

    ...

--- src/main/java/com/example/CloudDocs/controller/MediaController.java
+++ src/main/java/com/example/CloudDocs/controller/MediaController.java
@@ -1,6 +1,7 @@
 package com.example.CloudDocs.controller;
 
 import com.example.CloudDocs.dto.ApiResponse;
+import com.example.CloudDocs.service.CloudinaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/media")
@@ -16,7 +19,7 @@ public class MediaController {
 
     private final CloudinaryService cloudinaryService;
 
-    public ResponseEntity<ApiResponse> uploadFile(@RequestParam("file")MultipartFile file, @RequestParam(value = "description",required = false) String description){
+    public ResponseEntity<ApiResponse> uploadFile(@RequestParam("file")MultipartFile file, @RequestParam(value = "description",required = false) String description) throws IOException {
         if (file.isEmpty()){
             return ResponseEntity.badRequest().body(ApiResponse.error("File is empty"));
 
@@ -25,4 +28,9 @@ public class MediaController {
         return ResponseEntity.ok(ApiResponse.success("File uploaded successfully"));
 
     }
+public ResponseEntity<ApiResponse> deleteFile(@RequestParam("publicId") String publicId) throws IOException {
+    cloudinaryService.deleteFile(publicId);
+    return ResponseEntity.ok(ApiResponse.success("File deleted successfully"));
+
+}
 }
--- src/main/java/com/example/CloudDocs/service/CloudinaryService.java
+++ src/main/java/com/example/CloudDocs/service/CloudinaryService.java
@@ -1,12 +1,35 @@
 package com.example.CloudDocs.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class CloudinaryService {
-    public void uploadFile(){
+    private final Cloudinary cloudinary;
 
+
+    public Map<String,Object> uploadFile(MultipartFile file,String descripttion) throws IOException {
+        Map uploadRes=cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.asMap("folder","clouddocs/","description",descripttion)
+
+        );
+        return Map.of(
+                "url",uploadRes.get("secure_url"),"publicId",uploadRes.get("public_id")
+        );
+
+
+    }
+    public void deleteFile(String publicId) throws IOException {
+        cloudinary.uploader().destroy(publicId,ObjectUtils.emptyMap());
+        System.out.println("File deleted successfully");
+        
     }
 }
